b7ff21e2300237edb3998655cd485db66d012ee1
Dejan Stojanovic <d.stojanovic@hotmail.com>
2014-09-07 09:48:29 +0400

    optimized for minification

--- src/jquery.googlemaps.js
+++ src/jquery.googlemaps.js
@@ -1,7 +1,7 @@
 ﻿/*
  * jQuery Plugin: JQuery GoogleMaps
  * https://github.com/dejanstojanovic/JQuery-GoogleMaps
- * Version 2.2.1
+ * Version 2.2.3
  *
  * Copyright (c) 2014 Dejan Stojanovic (http://dejanstojanovic.net)
  *
